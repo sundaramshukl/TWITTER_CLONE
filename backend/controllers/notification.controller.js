a4ecdc1935d01c7f95c6c8790a0f200d7763e2ec
sundaramshukl <ssundaram2004@gmail.com>
2025-04-06 22:08:34 +0530

    fix: remove unnecessary whitespace in getNotifications function

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -11,7 +11,7 @@ export const getNotifications = async (req, res) => {
     } catch (error) {
         console.error("Error fetching notifications:", error);
         res.status(500).json({ error: "Internal server error" }); // Handle errors
-        
+    
     }
 }
 export const deleteNotifications = async (req, res) => {
